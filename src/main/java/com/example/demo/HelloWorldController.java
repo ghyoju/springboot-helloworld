731db1e21c7a941e3fdafa66d006eb06442c5d62
ghyoju <ghyoju@hotmail.com>
2026-07-06 12:49:24 -0500

    Initial Commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Marking this class as a REST controller
+@RestController
+public class HelloWorldController { 
+
+    // Mapping the root URL ("/") to this method
+    @RequestMapping("/") 
+    public String helloWorld() { 
+        
+        // Returning a simple "Hello World" response
+        return "Hello World"; 
+    } 
+}
